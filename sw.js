5bfd3ea3b7526e2d37b5f62a5d11b5eef543eb2f
SmejjCom <smejjCom@gmail.com>
2026-07-02 16:56:51 +0200

    fix: SW-Precache v62 umgeht HTTP-Cache (cache reload) gegen veraltete Offline-Shell

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "smejj-shell-v61";
+const CACHE_NAME = "smejj-shell-v62";
 const SHELL = [
   "/",
   "/assets/styles.css",
@@ -34,7 +34,7 @@ const SHELL = [
 ];
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(SHELL)));
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(SHELL.map((url) => new Request(url, { cache: "reload" })))));
   self.skipWaiting();
 });
 
